d925745ee1e476d8832fe985c1f3fff767e78e1b
Herman Bergwerf <hermanbergwerf@gmail.com>
2017-03-14 15:05:36 +0100

    Add default library

---
+++ lib/eqdb_client.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2017, Herman Bergwerf. All rights reserved.
+// Use of this source code is governed by an AGPL-3.0-style license
+// that can be found in the LICENSE file.
+
+library eqdb_client;
+
+export 'eqdb_client_v0.dart';
